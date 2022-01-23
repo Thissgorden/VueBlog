eb35326abbb83ce76f97b1a1ad6dc14f8fc4f448
gdlearn <690319344@qq.com>
2022-01-23 12:37:03 +0800

    默认变更列表

---
+++ gdlearnvue/src/views/UserCenter/Registry.vue
@@ -0,0 +1,23 @@
+<template>
+<el-container>
+    注册界面 开发中
+</el-container>
+</template>
+
+<script>
+export default {
+  name: "Registry",
+  data() {
+    return {
+      
+    };
+  },
+  created(){
+
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
